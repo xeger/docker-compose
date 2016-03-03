ce92c2ff6e9b7f27c038e0d54a7883be1f5f7efd
Sam Stokes <me@samstokes.co.uk>
2016-03-03 15:00:12 -0800

    Fix "Backticks::Runner#command is deprecated" warnings

--- spec/docker/compose/session_spec.rb
+++ spec/docker/compose/session_spec.rb
@@ -12,22 +12,22 @@ describe Docker::Compose::Session do
 
   before do
     allow(status).to receive(:success?).and_return(exitstatus == 0)
-    allow(shell).to receive(:command).and_return(command)
+    allow(shell).to receive(:run).and_return(command)
     allow(command).to receive(:join).and_return(command)
   end
 
   describe '.new' do
     it 'allows file override' do
       s1 = described_class.new(shell, file: 'foo.yml')
-      expect(shell).to receive(:command).with('docker-compose', {file: 'foo.yml'}, anything, anything, anything)
+      expect(shell).to receive(:run).with('docker-compose', {file: 'foo.yml'}, anything, anything, anything)
       s1.up
     end
   end
 
   describe '#up' do
     it 'runs containers' do
-      expect(shell).to receive(:command).with('docker-compose', anything, 'up', anything, anything)
-      expect(shell).to receive(:command).with('docker-compose', anything, 'up', hash_including(d:true), anything)
+      expect(shell).to receive(:run).with('docker-compose', anything, 'up', anything, anything)
+      expect(shell).to receive(:run).with('docker-compose', anything, 'up', hash_including(d:true), anything)
       session.up
       session.up detached:true
     end
